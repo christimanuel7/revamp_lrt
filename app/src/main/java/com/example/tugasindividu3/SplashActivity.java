c5aa6799d8074422c98ee035829a04c3e7161c3e
Christopher Imanuel <christ.imanuel7@gmail.com>
2022-11-11 02:46:24 +0700

    Basic Replace Fragment Done (2)

--- app/src/main/java/com/example/tugasindividu3/SplashActivity.java
+++ app/src/main/java/com/example/tugasindividu3/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.tugasindividu3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //      MEMUNCULKAN SPLASH PAGE SELAMA 1500 ms, LANJUT LOGIN INTENT
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent login = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(login);
+            }
+        },1500);
     }
 }
