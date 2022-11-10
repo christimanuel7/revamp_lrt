f5beb6c95ffb494b05b867de057af07d809ec285
Christopher Imanuel <christ.imanuel7@gmail.com>
2022-11-11 02:26:18 +0700

    Basic Replace Fragment Done

--- app/src/main/java/com/example/tugasindividu3/MainActivity.java
+++ app/src/main/java/com/example/tugasindividu3/MainActivity.java
@@ -4,18 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -26,50 +28,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        drawerLayout=findViewById(R.id.drawer_layout);
-        navigationView=findViewById(R.id.navigation_view);
-        toolbar=findViewById(R.id.toolbar);
-
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_open,R.string.navigation_close);
-
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_open, R.string.navigation_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId())
-                {
-                    case R.id.menu_beranda:
-                    {
-                        break;
-                    }
-                    case R.id.menu_rute:
-                    {
-                        break;
-                    }
-                    case R.id.menu_jadwal:
-                    {
-                        break;
-                    }
-                    case R.id.menu_tentang:
-                    {
-                        break;
-                    }
-                    case R.id.menu_hubungi:
-                    {
-                        break;
-                    }
-                }
-                return true;
-            }
-        });
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.bringToFront();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new BerandaFragment()).commit();
+            navigationView.setCheckedItem(R.id.menu_beranda);
+        }
     }
 
-    private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager=getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menu_beranda:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new BerandaFragment()).commit();
+                break;
+            case R.id.menu_rute:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new RuteFragment()).commit();
+                break;
+            case R.id.menu_jadwal:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new JadwalFragment()).commit();
+                break;
+            case R.id.menu_tentang:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new TentangFragment()).commit();
+                break;
+            case R.id.menu_hubungi:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HubungiFragment()).commit();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
 }
