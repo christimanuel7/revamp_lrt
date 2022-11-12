ed125191fb6f1dbeed1bc59e10b05b24dd98c6b4
Christopher Imanuel <christ.imanuel7@gmail.com>
2022-11-12 22:23:29 +0700

    12/11/2022 22:23

--- app/src/main/java/com/example/tugasindividu3/RuteFragment.java
+++ app/src/main/java/com/example/tugasindividu3/RuteFragment.java
@@ -3,12 +3,16 @@ package com.example.tugasindividu3;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class RuteFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
